b11f6a3260b036fb47b15ca5911848d0f927740a
James McTighe <james.mctighe@lanelink.com>
2025-12-27 11:06:36 -0500

    initial commit

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,22 @@
+
+import { useEffect } from "react"
+
+export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
+
+  return (
+    <div className={`
+      fixed top-0 left-0 w-full bg-[rega(10,10,10, 0.8] z-40 flex flex-col items-center justify-center 
+      transition-all duration-300 ease-in-out 
+      ${menuOpen
+        ? "h-screen opacity-100 pointer-event-auto"
+        : "h-0 opacity-0 pointer-event-none"
+      }
+    `}>
+
+      <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer">
+        &times;
+      </button>
+
+    </div>
+  );
+};
