511ba9bb7d85967128fae567931e408c8519cde6
James McTighe <james.mctighe@lanelink.com>
2026-03-30 11:48:43 -0400

    updated contact page

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="rounded-xl text-2xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 
           to-cyan-400 bg-clip-text text-transparent text-center"
           >
@@ -16,50 +16,24 @@ export const Contact = () => {
           <form className="space-y-6">
 
             <div className="relative">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                className="w-full bg-white/5 border border-white/30 rounded px-4 py-3 text-white transition 
-              focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Name..."
-              />
+              <h3>Email: <br/>james.mctighe97@gmail.com</h3>
             </div>
 
             <div className="relative">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                className="w-full bg-white/5 border border-white/30 rounded px-4 py-3 text-white transition 
-              focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="example@gmail.com"
-              />
+              <h3>Phone: <br/>810-623-8291</h3>
             </div>
 
             <div className="relative">
-              <textarea
-                id="message"
-                name="message"
-                required
-                rows={5}
-                className="w-full bg-white/5 border border-white/30 rounded px-4 py-3 text-white transition 
-              focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Your Message..."
-              />
+              <h3>Linkedin:<br/>
+                <a
+                  href="https://linkedin.com/in/james-mctighe"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  linkedin.com/in/james-mctighe
+                </a>
+              </h3>
             </div>
 
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium 
-              transition duration-300 ease-in-out relative overflow-hidden hover:bg-green-700 
-              hover:-translate-y-0.5
-              hover:shadow-[0_0_15px_rgba(59, 130,246)]"
-            >
-              Send Message!
-            </button>
           </form>
         </div>
       </RevealOnScroll>
